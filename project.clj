099a172aa0cef1d2e428d731aa926df9756d7590
Tom Brennan <tjb1982@gmail.com>
2015-11-03 14:04:43 +0000

    changed into a library instead of a cli

--- project.clj
+++ project.clj
@@ -4,16 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-;                 [selmer "0.9.2"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [stencil "0.5.0"]
                  [cheshire "5.5.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [circleci/clj-yaml "0.5.3"]
                  [me.raynes/conch "0.8.0"]
-                 [org.clojure/java.jdbc "0.3.2"]
-                 [org.postgresql/postgresql "9.4-1200-jdbc41"]
-                 [flax-file-connector "0.1.0-SNAPSHOT"]
-                 [flax-lxc-connector "0.1.0-SNAPSHOT"]
                  ]
   :aot :all
-  :main flax.main)
+  :main flax.core)
