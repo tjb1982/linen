00b4476a10ad6c47a9a813fe2ee11945944bac10
Tom Brennan <tjb1982@gmail.com>
2015-12-19 20:34:32 -0500

    refactored/centralized the invocation from the node-connector to the manager

---
+++ src/co/nclk/flax/data.clj
@@ -0,0 +1,28 @@
+(ns co.nclk.flax.data
+  (:require [clj-yaml.core :as yaml])
+  (:gen-class))
+
+
+(defprotocol PDataConnector
+  (resolve-program [self m])
+  (resolve-module [self m]))
+
+
+(defn slurp-yaml
+  [s]
+  (let [s (if (.startsWith s (str "~" java.io.File/separator))
+            (clojure.string/replace s #"~" (str (System/getProperty "user.home")))
+            s)]
+    (-> s slurp yaml/parse-string)))
+
+
+(defrecord FileDataConnector []
+  PDataConnector
+  (resolve-program [self s]
+    (slurp-yaml s))
+  (resolve-module [self s]
+    (slurp-yaml s)))
+ 
+
+(defn connector [] (FileDataConnector.))
+
