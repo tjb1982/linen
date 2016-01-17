fbf6fbab7937390b948b387bd352fc017e45afd3
Tom Brennan <tjb1982@gmail.com>
2016-01-17 11:07:29 -0500

    more readme

--- test/co/nclk/flax/core_test.clj
+++ test/co/nclk/flax/core_test.clj
@@ -16,3 +16,19 @@
       (-> r :failures count zero? is)
       (-> r :env nil? is)
       )))
+
+(deftest test-minimal-with-python
+  (testing "A minimal configuration using various interpreters"
+    (let [invocations ["#!/usr/bin/env python2\nprint 'Hello Python'"
+                       "#!/usr/bin/env tclsh\nputs {Hello Tcl}"
+                       "#!/usr/bin/env node\nconsole.log('Hello JavaScript');"]
+          r (flax/run {:program
+                       {:main
+                        [{:module
+                          {:checkpoints
+                           [(map #(identity {:invocation %}) invocations)]}}]}})]
+      (-> r :raw nil? not is)
+      (-> r :checkpoints count (= (count invocations)) is)
+      (-> r :failures count zero? is)
+      (-> r :env nil? is)
+      )))
