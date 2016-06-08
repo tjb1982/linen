c0fb3beea1374f8699fdd1bd4bb128d690ea2f91
Tom Brennan <tjb1982@gmail.com>
2016-06-08 10:58:29 -0400

    removed redundant code

--- src/co/nclk/linen/core.clj
+++ src/co/nclk/linen/core.clj
@@ -1,6 +1,5 @@
 (ns co.nclk.linen.core
-  (:require ;;[clojure.core.async :as a :refer [chan go >! >!! <! <!! alts!! alts! go-loop thread]]
-            [clj-yaml.core :as yaml]
+  (:require [clj-yaml.core :as yaml]
             [cheshire.core :as json]
             [cheshire.generate :refer [add-encoder encode-str]]
             [clojure.pprint :refer [pprint]]
@@ -9,6 +8,7 @@
             [co.nclk.linen.data :refer [resolve-module resolve-program]]
             [stencil.parser :refer [parse]]
             [stencil.core :refer [render]]
+            [co.nclk.flax.core :as flax]
             )
   (:import co.nclk.linen.data.FileDataConnector)
   (:gen-class))
@@ -55,62 +55,6 @@
         (-> key* keyword env)))
     (catch NumberFormatException nfe
       (-> key* keyword env))))
-
-
-(defn swap
-  [s env]
-  (let [env (merge @genv env)]
-    (cond
-
-      ;; Env dump.
-      (= (clojure.string/trim s) "~@")
-      env
-
-      ;; Support for keywords.
-      (.startsWith s "~:")
-      (keyword (subs s 2))
-
-      ;; Support for symbols
-      (.startsWith s "~'")
-      (symbol (subs s 2))
-
-      ;; If `s` starts with ~@, then replace it with the data held by the
-      ;; variable (i.e., not a string interpolation of it).
-      (.startsWith s "~@")
-      (let [target (-> s (subs 2))]
-        (loop [target target env env]
-          (when-not (or (nil? env) (empty? env))
-            (cond
-              (-> target (.contains "."))
-              (let [parts (-> target (clojure.string/split #"\." 2))]
-                ;; TODO support bracket sugar (e.g., foo[0].bar[baz])
-                (recur (second parts)
-                       (get-with-string-maybe-index (first parts) env)))
-              :else
-              (when-not (or (empty? env)
-                            (clojure.string/blank? target))
-                (let [data (get-with-string-maybe-index target env)]
-                  (if (string? data) (clojure.string/trim data) data)))))))
-
-      ;; If `s` starts with "~$", then replace it with the
-      ;; stdout result of running the script locally.
-      (.startsWith s "~$")
-      (let [s (subs s 2)
-            proc (-> (Runtime/getRuntime)
-                   (.exec (into-array String
-                            ["bash" "-c" (clojure.string/trim s)])))
-            stdout (clojure.java.io/reader (.getInputStream proc))]
-        (clojure.string/join "\n"
-          (loop [lines []]
-            (let [line (.readLine stdout)]
-              (if (nil? line)
-                lines
-                (recur (conj lines line)))))))
-
-      ;; Interpolate.
-      :else (render
-              (parse s @parser-options)
-              env))))
 
 
 (declare evaluate)
@@ -348,14 +292,6 @@
   [m config]
   (let [config (assoc config :env (merge @genv (:env config)))]
     (cond
-      (string? m)
-      (swap m (:env config))
-
-      (keyword? m)
-      (keyword (swap (subs (str m) 1) (:env config)))
-
-      (symbol? m)
-      (symbol (swap (name m) (:env config)))
 
       (map? m)
       (cond
@@ -382,112 +318,7 @@
 
         ;; Functions and special forms
         (->> (keys m) (some #(-> % str (subs 1) (.startsWith "~("))))
-        (let [fun-entry (->> m (filter #(-> % key str (subs 1) (.startsWith "~("))) first)
-              fun (-> fun-entry key str (subs 3) symbol)
-              do-statements (fn [statements env]
-                              (loop [statements statements last-ret nil]
-                                (if (empty? statements)
-                                  last-ret
-                                  (let [ret (evaluate (first statements) (assoc config :env env))]
-                                    (recur (drop 1 statements) ret)))))]
-          (condp = fun
-            ;; Special forms
-            'if
-            (evaluate
-              ((if (evaluate (-> fun-entry val first) config)
-                 second #(nth % 2 nil))
-                (-> fun-entry val))
-              config)
-
-            'let
-            (let [bindings (-> fun-entry val first)]
-              (if-not (even? (count bindings))
-                (throw (IllegalArgumentException. "`let` requires an even number of bindings"))
-                (let [bindings (-> fun-entry val first)
-                      statements (->> fun-entry val (drop 1))
-                      env (loop [bindings bindings
-                                 env (:env config)]
-                            (if (empty? bindings)
-                              env
-                              (recur (drop 2 bindings)
-                                     (merge env
-                                            {(evaluate (keyword (first bindings)) config)
-                                             (evaluate (second bindings) config)}))))]
-                  (do-statements statements env))))
-
-            'fn
-            (fn [& argv]
-              (let [args (-> fun-entry val first) ;; list of strings
-                    statements (->> fun-entry val (drop 1))
-                    env (loop [args args
-                               argv argv
-                               env (:env config)]
-                          (if (empty? args)
-                            env
-                            (let [env (merge env
-                                             {(evaluate (keyword (first args)) config)
-                                              (evaluate (first argv) config)})]
-                              (recur (drop 1 args)
-                                     (drop 1 argv)
-                                     env))))]
-                (do-statements statements env)))
-
-            (symbol "#")
-            (fn [& argv]
-              (let [env (merge (:env config)
-                               (into {}
-                                 (map-indexed
-                                   (fn [idx item] [(keyword (str idx)) (evaluate item config)])
-                                   argv)))]
-                (do-statements (-> fun-entry val) env)))
-
-            'for
-            (let [coll (-> fun-entry val first second (evaluate config))]
-              (doall
-                (map
-                  #(let [env (merge (:env config) {(keyword (-> fun-entry val ffirst (evaluate config))) %})]
-                    (evaluate (->> fun-entry val (drop 1)) (assoc config :env env)))
-                  coll)))
-
-            'or
-            (loop [args (-> fun-entry val)]
-              (when-not (empty? args)
-                (let [yield (evaluate (first args) config)]
-                  (if-not yield
-                    (recur (drop 1 args))
-                    yield))))
-
-            'and
-            (loop [args (-> fun-entry val)
-                   last-yield nil]
-              (if (empty? args)
-                last-yield
-                (let [yield (evaluate (first args) config)]
-                  (if-not yield
-                    false
-                    (recur (drop 1 args) yield)))))
-
-            'parallel
-            (upmap #(evaluate % config)
-              (-> fun-entry val))
-
-            'upmap
-            (apply upmap (evaluate (-> m first val) config))
-
-            'log
-            (let [args (-> fun-entry val)]
-              (log (evaluate (-> args first keyword) config)
-                   (apply str
-                     (map #(evaluate % config) (drop 1 args)))))
-
- 
-            ;; Functions
-            (let [yield (apply (resolve fun) (evaluate (-> m first val) config))]
-              (if (coll? yield)
-                ;; Laziness disabled.
-                (doall yield)
-                yield))))
-
+        (flax/evaluate m config evaluate)
 
         ;; Modules
         (contains? m :module)
@@ -520,13 +351,7 @@
                    [(evaluate k config) (evaluate v config)]))
                m)))
 
-      ;; Other collections
-      (coll? m)
-      ;;((if (seq? m) reverse identity)
-      (into (if (seq? m) [] (empty m))
-            (doall (map (fn [item] (evaluate item config)) m)))
-      
-      :else m)))
+      :else (flax/evaluate m config evaluate))))
 
 
 (defn run-program
