4c625b1f9346a5af7fd934152ef8ebd38f0dcedf
Tom Brennan <tjb1982@gmail.com>
2017-01-04 10:44:03 -0500

    handler connector

---
+++ src/co/nclk/linen/connector/handler.clj
@@ -0,0 +1,14 @@
+(ns co.nclk.linen.connector.handler
+  (:require [clj-yaml.core :as yaml]
+            [co.nclk.linen.data :as data])
+  (:gen-class))
+
+(defrecord HandlerDataConnector [phandler mhandler]
+  data/PDataConnector
+  (resolve-program [self s]
+    (phandler s))
+  (resolve-module [self s]
+    (mhandler s)))
+
+(defn connector [phandler mhandler]
+  (HandlerDataConnector. phandler mhandler))
