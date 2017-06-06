38abcd607139d34bf1bd9a325d7e98cfb6f884a1
Tom Brennan <tjb1982@gmail.com>
2017-06-06 13:45:25 -0400

    version 2.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject co.nclk/linen "2.1.1"
+(defproject co.nclk/linen "2.1.2"
   :description "Implementation of an interpreter for flax, a domain specific
                language for modeling coordinated, distributed processes."
   :url "https://github.com/tjb1982/linen"
