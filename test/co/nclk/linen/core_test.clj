c5d9ed103f3a34cac91626b0ed087ba463714929
Tom Brennan <tjb1982@gmail.com>
2018-05-27 18:59:30 -0400

    better data model for Checkpoint, Module, and Parent/Child

--- test/co/nclk/linen/core_test.clj
+++ test/co/nclk/linen/core_test.clj
@@ -17,7 +17,7 @@
   (-> name* clojure.java.io/resource slurp yaml/parse-all))
 
 
-(def config-keyset #{:effective :main})
+(def config-keyset-stub #{:effective :main})
 (def seed 8031)
 (def base-config
   {:effective seed
@@ -26,7 +26,7 @@
    :failed? (atom false)})
 
 
-(deftest config-pre-check
+(deftest config-pre-check-test
   (testing "config-pre-check works in isolation"
     (let [r (linen/config-pre-check {:effective 1234 :main [{:name ""} {}]})
           r2 (try
@@ -34,7 +34,7 @@
                (catch IllegalArgumentException iae iae))]
       (-> r nil? is)
       (->> r2 (instance? IllegalArgumentException) is)
-      (is (every? #(.contains (-> r2 .getMessage) (name %)) config-keyset))))
+      (is (every? #(.contains (-> r2 .getMessage) (name %)) linen/config-keyset))))
 
   (testing "config-pre-check works in a program run"
     (let [r (linen/run {:effective 8031 :main [{:name ""} {}]})]
@@ -42,7 +42,7 @@
 
 
 (deftest config-keyset-test
-  (-> config-keyset (= linen/config-keyset) is))
+  (-> config-keyset-stub (= linen/config-keyset) is))
 
 
 (deftest normalize-module-test
@@ -86,6 +86,112 @@
             module (linen/normalize-module (first tc))]
         (testing (yaml/generate-string {tcidx tc})
           (-> module (= (second tc)) is))))))
+
+
+(deftest extract-module-env-test
+  ;; argv: [provides body input-env]
+  ;; expected: {...some environment}
+  (let [test-cases
+        [{:label (str "When `provides` is nil, and nothing else is provided, it should "
+                      "return an empty map.")
+          :argv [nil]
+          :expected {}}
+         {:label (str "When `provides` is nil, body is nil, and input-env is nil, it should "
+                      "return an empty map.")
+          :argv [nil nil nil]
+          :expected {}}
+         {:label (str "When `provides` is an empty vector, body is an empty vector, and "
+                      "input-env is nil, it should return an empty map.")
+          :argv [[] [] nil]
+          :expected {}}
+         {:label (str "When `provides` is an empty map, body is an empty map, and input-env "
+                      "is an empty map, it should return an empty map.")
+          :argv [{} {} {}]
+          :expected {}}
+         {:label (str "When `provides` is nil, body is nil, and input-env is anything other "
+                      "than a map, it should return an empty map.")
+          :argv [nil nil []]
+          :expected {}}
+         {:label (str "When `provides` is nil, body is nil, and input-env is anything other "
+                      "than a map, it should return an empty map.")
+          :argv [nil nil ""]
+          :expected {}}
+         {:label (str "When `provides` is nil, body is nil, and input-env is anything other "
+                      "than a map, it should return an empty map.")
+          :argv [nil nil 123]
+          :expected {}}
+         {:label (str "When `provides` is not a collection, body is not a collection, and "
+                      "input-env is anything other than a map, it should return an empty map.")
+          :argv [123 123 123]
+          :expected {}}
+         {:label (str "When `provides` is nil, body has an env, and input-env is empty, it "
+                      "should return an empty map.")
+          :argv [nil [{:checkpoint [] :env {:a "b"}}] {}]
+          :expected {}}
+         {:label (str "When `provides` is nil, body has an env, and input-env has entries, it "
+                      "should return the entire input-env and nothing else.")
+          :argv [nil [{:checkpoint [] :env {:a "b"}}] {:c "d"}]
+          :expected {:c "d"}}
+         {:label (str "When `provides` is not nil, body has at least one env, and input-env "
+                      "is empty, it should return an empty map.")
+          :argv [nil [{:checkpoint [] :env {:a "b"}}] {}]
+          :expected {}}
+         {:label (str "When `provides` is a map containing entries with literal values, body is "
+                      "nil, and input-env is nil, it should return an env comprised of the "
+                      "`provides` entries.")
+          :argv [{:a "b"} {} {}]
+          :expected {:a "b"}}
+         {:label (str "When `provides` is a map containing an entry with a literal value, "
+                      "body has an env containing an entry with a key found in the `provides` "
+                      "but with a different value, and input-env is nil, it should return an "
+                      "env comprised of the `provides` entries.")
+          :argv [{:a "b"} {:env {:a "c"}} {}]
+          :expected {:a "b"}}
+         {:label (str "When `provides` contains a string that matches the key of any entry "
+                      "in the body env, it should return the entry from the body env")
+          :argv [["a"] {:env {:a "b"}}]
+          :expected {:a "b"}}
+         {:label (str "When `provides` contains a map containing a key that matches any entry "
+                      "in the body env, and a value that `~@` references an entry in the body "
+                      "env, it should return the `~@`-value of the entry of the body env into the "
+                      "key represented in the `provides` entry.")
+          :argv [{:a "~@b"} {:env {:b "c"}}]
+          :expected {:a "c"}}
+         {:label (str "When the `provides` arg is a sequencial collection, each of its items "
+                      "is `(reduce merge)`-ed.")
+          :argv [[{:a "b"} {:b "~@a"} {:c "~@b"} {:d "~@a"}]]
+          :expected {:a "b" :b "b" :c "b" :d "b"}}
+         {:label (str "When `provides` is a sequential collection, each of its items can be "
+                      "any appropriate type.")
+          :argv [[{:a "b"} "c" {:key "d" :value "~@c"}] {:env {:c "d"}}]
+          :expected {:a "b" :c "d" :d "d"}}
+         {:label (str "Conflicts between the body env and the input-env should allow the "
+                      "body env to win.")
+          :argv [{:a "~@a" :b "~@b"} {:env {:a "b"}} {:a "z" :b "y"}]
+          :expected {:a "b" :b "y"}}
+         {:label (str "When `provides` is empty, only the input-env should pass through.")
+          :argv [nil [{:env {:a "b" :c "d"}}] {:a "z" :b "y"}]
+          :expected {:a "z" :b "y"}}
+         {:label (str "When `provides` has a binding, only that binding should change the "
+                      "resulting env, the rest of the input-env should pass through.")
+          :argv [[:a] [{:env {:a "b" :c "d"}}] {:a "z" :b "y"}]
+          :expected {:a "b" :b "y"}}
+         {:label (str "When `provides` is just a string, it's treated as a key/value binding.")
+          :argv ["a" {:env {:a "b"}}]
+          :expected {:a "b"}}
+         {:label (str "When `provides` has a simple binding and the body contains multiple "
+                      "envs with the same key, last out wins (i.e., reverse order).")
+          :argv ["a" [{:env {:a "b"}} {:env {:a "d"}}]]
+          :expected {:a "b"}}
+         {:label (str "When `provides` has a simple binding and the body contains multiple "
+                      "envs with the same key, last out wins (i.e., reverse order).")
+          :argv ["a" [{:foo [{:bar {:env {:a "z"}}} {:env {:a "b"}}]} {:foo {:env {:a "d"}}}] {:a "y"}]
+          :expected {:a "z"}}
+        ]]
+    (doseq [{:keys [label argv expected]} test-cases]
+      (let [actual (apply linen/extract-module-env argv)]
+        (testing label
+          (is (= expected actual)))))))
 
 
 (deftest run-module-test-basic
@@ -151,7 +257,7 @@
 (deftest run2-test
   (do-run-test "run2.yaml"))
 
-(deftest provides
+(deftest provides-test
   (let [module (-> "out.yaml" slurp-test)
         r (linen/evaluate module (-> base-config (assoc :env {:FOO {:name "bar"}})))]
     ;;(clojure.pprint/pprint r)
